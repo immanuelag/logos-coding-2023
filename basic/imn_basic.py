7c7e2724e37c8d04e80a2df59533e69a549727ce
Immanuel Agustinus Gunawan <immanuel.gunawan@infoglobal.co.id>
2023-10-02 14:14:54 +0700

    add imn_basic.py file

---
+++ basic/imn_basic.py
@@ -0,0 +1,34 @@
+# == 
+alas = 4 # a adlah variabel alas segitiga
+tinggi = 4 # b adalah variabel tinggi segitiga
+#print('hi')
+#print('good morning')
+# cek nilai itu lebih kecil dari 100 dan lebih besar 85
+A = 90 
+B = 81 
+'''
+print("A adalah ", (A < 100) and (A > 85))
+print("B adalah ", (B < 100) and (B > 85))
+'''
+'''
+A=70
+if (A < 80):
+    if(A < 60):
+        print("Benar2")  
+        if(A < 40):
+            print("Benar3")
+        else:
+            print("Salah3")
+    else:
+        print("Salah2")
+    print("Bener1")
+else:
+    print("Salah1")
+'''
+i = 0
+while (i < 5):
+    print("sebelum ", i)
+    i = i + 1
+    if (i == 2):
+        continue
+    print("sesudah ", i)
